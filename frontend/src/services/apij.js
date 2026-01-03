36dcf1a527b7a2fbfd84a39f129658ad67f5011b
praise936 <peterpraise259@gmail.com>
2026-01-03 23:27:09 +0300

    Configure React for production API

--- frontend/src/services/apij.js
+++ frontend/src/services/apij.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apij = axios.create({
-    baseURL: "http://localhost:8000/api/",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 apij.interceptors.request.use((config) => {
